147f1bff5a4e88d9d1a0b874f8435f08d693ffe4
louismachin <louis@machin.dev>
2026-06-08 20:22:01 +0100

    Fixed consolidate methods

--- lib/consolidate.rb
+++ lib/consolidate.rb
@@ -1,6 +1,10 @@
-def consolidated_data(latitude, longitude, year, day, month, hour, minute, timezone_offset)
+def consolidated_data(latitude, longitude, year, month, day, hour, minute, timezone_offset)
     return {
         :sidereal => sidereal_chart_values(latitude, longitude, year, month, day, hour, minute, timezone_offset),
         :tropical => tropical_chart_values(latitude, longitude, year, month, day, hour, minute, timezone_offset),
     }
+end
+
+def consolidated_data_by_datetime(latitude, longitude, datetime)
+    return consolidated_data_by_datetime(latitude, longitude, *time_to_gregorian_datetime_values(datetime))
 end
